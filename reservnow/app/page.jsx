d487d921394aba7b2465fc14ada74fabbef03ad8
Olusanya <olusanyajolaoluwa@gmail.com>
2023-06-08 17:07:07 +0100

    Login Done

--- reservnow/app/page.jsx
+++ reservnow/app/page.jsx
@@ -1,10 +1,15 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./page.module.css";
+import LoginModal from "@/modals/Login Modal/LoginModal";
 
 export default function Home() {
   return (
-    <main style={{backgroundColor:"white", color:"black"}}>
-      <h1>Hello</h1>
+    <main style={{ backgroundColor: "white", color: "black" }}>
+      <div>
+        <LoginModal />
+      </div>
     </main>
   );
 }
