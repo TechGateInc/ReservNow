f3043d7af72a1e9938ad6239128f81939a775c6b
Nasiru Iyidemilade <ladenas202@gmail.com>
2023-06-08 13:31:13 +0100

    folder structure

---
+++ reservnow/app/page.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main style={{backgroundColor:"white", color:"black"}}>
+      <h1>Hello</h1>
+    </main>
+  );
+}
