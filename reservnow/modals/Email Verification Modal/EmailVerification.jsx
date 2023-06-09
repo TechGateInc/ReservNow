766b7298b09e43d42392e3b39e346aaefced6917
Olusanya <olusanyajolaoluwa@gmail.com>
2023-06-09 11:30:43 +0100

    error fix

--- reservnow/modals/Email Verification Modal/EmailVerification.jsx
+++ reservnow/modals/Email Verification Modal/EmailVerification.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import styles from "./emailverification.module.css";
+import styles from "./emailVerification.module.css";
 
 export default function EmailVerification({
   formData,
