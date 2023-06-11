1562226c02593b6927d8fa9515ddaea64b6cd544
Nasiru Iyidemilade <ladenas202@gmail.com>
2023-06-11 07:56:53 +0100

    fixes

--- reservnow/components/CentreGallery/CentreCard.jsx
+++ reservnow/components/CentreGallery/CentreCard.jsx
@@ -1,41 +1,58 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import "../CentreGallery/CentreGallery.css";
-import { AiFillStar, AiOutlineHeart, AiFillHeart } from "react-icons/ai";
+import {
+  AiFillStar,
+  AiOutlineHeart,
+  AiFillHeart,
+  AiFillCreditCard,
+} from "react-icons/ai";
 import ImageSlider from "./ImageSlider";
+import { BsPeopleFill } from "react-icons/bs";
+import { MdLocationOn } from "react-icons/md";
 
-const CentreCard = ({centre}) => {
+const CentreCard = ({ centre }) => {
   const [isImageClicked, setIsImageClicked] = useState(false);
 
   const handleClick = () => {
     setIsImageClicked(!isImageClicked);
   };
-  return(
-  <div className="galleryCardV1">
-    <div className="centreImageholder">
-      <div className="iconWrapper" onClick={handleClick}>
-        {!isImageClicked ? (
-          <AiOutlineHeart className="likeIcon1" />
-        ) : (
-          <AiFillHeart className="likeIcon2" />
-        )}
+  return (
+    <div className="galleryCardV1">
+      <div className="centreImageholder">
+        <div className="iconWrapper" onClick={handleClick}>
+          {!isImageClicked ? (
+            <AiOutlineHeart className="likeIcon1" />
+          ) : (
+            <AiFillHeart className="likeIcon2" />
+          )}
+        </div>
+        <ImageSlider />
       </div>
-      <ImageSlider />
+      <div className="centreDesc">
+        <div>
+          <p style={{ fontSize: 20, fontFamily: "700" }}>{centre.centreName}</p>
+          <div className="centreDesc-content" style={{marginTop:5}}>
+            <MdLocationOn />
+            <p style={{ color: "gray", marginLeft:10 }}>
+              {centre.centreCity}, {centre.centreState}
+            </p>
+          </div>
+          <div className="centreDesc-content">
+            <BsPeopleFill /> 
+            <p style={{ color: "gray", marginLeft:10 }}>{centre.capacity}</p>
+          </div>
+          <div className="centreDesc-content">
+            <AiFillCreditCard />{" "}
+            <p style={{ color: "gray", marginLeft:10 }}>₦{centre.price}/hr</p>
+          </div>
+        </div>
+        <div className="ratingSection">
+          <AiFillStar />
+          4.7
+        </div>
+      </div>
     </div>
-    <div className="centreDesc">
-      <div>
-        <p style={{ fontSize: 20, fontFamily: "700" }}>{centre.centreName
-}</p>
-        <p style={{ color: "gray" }}>Lagos1 Nigeria</p>
-        <p style={{ color: "gray" }}> {centre.capacity}</p>
-        <p style={{ color: "gray" }}>{centre.price}/hr</p>
-      </div>
-      <div className="ratingSection">
-        <AiFillStar />
-        4.7
-      </div>
-    </div>
-  </div>
   );
 };
 
--- reservnow/components/CentreGallery/ImageSlider.jsx
+++ reservnow/components/CentreGallery/ImageSlider.jsx
@@ -1,23 +1,29 @@
-import React, { useState } from "react";
+"use client"
+import React, { useState, useRef } from "react";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/splide/dist/css/splide.min.css";
 
 const ImageSlider = () => {
   const [showArrows, setShowArrows] = useState(false);
+  const [autoplay, setAutoplay] = useState(true);
+  const splideRef = useRef(null);
 
   const splideOptions = {
     type: "loop",
     perPage: 1,
-    autoplay: true,
-    arrows: showArrows, // Set the initial value based on showArrows state
+    autoplay,
+    arrows: showArrows,
   };
 
   const handleMouseEnter = () => {
     setShowArrows(true);
+    setAutoplay(true); // Stop autoplay on hover
   };
 
   const handleMouseLeave = () => {
     setShowArrows(false);
+    setAutoplay(true); // Resume autoplay when leaving hover
+    splideRef.current.go("+1"); // Move to the next slide after resuming autoplay
   };
 
   return (
@@ -26,7 +32,7 @@ const ImageSlider = () => {
       onMouseLeave={handleMouseLeave}
       style={{ position: "relative" }}
     >
-      <Splide options={splideOptions}>
+      <Splide options={splideOptions} ref={splideRef}>
         <SplideSlide>
           <img
             src="images/TestImage.jpg"
