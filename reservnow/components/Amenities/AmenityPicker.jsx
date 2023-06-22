5469ac45b33b579b6e1145954bc9ab82b713e97a
Nasiru Iyidemilade <ladenas202@gmail.com>
2023-06-23 00:12:53 +0100

    fixes

--- reservnow/components/Amenities/AmenityPicker.jsx
+++ reservnow/components/Amenities/AmenityPicker.jsx
@@ -3,7 +3,50 @@ import "./AmenityPicker.css"
 
 const AmenityPicker = ({}) => {
   return <div className="AmmenityHolder">
-    Demilade
+    <p style={{fontSize:"35px"}}>What type of centre best describes your Event Centre ?</p>
+    <div className="centreTypeHolder">
+        <div className="centreTypeCard">
+            <div>icon</div>
+            <p>Television</p>
+        </div>
+        <div className="centreTypeCard">
+            <div>icon</div>
+            <p>Television</p>
+        </div>
+        <div className="centreTypeCard">
+            <div>icon</div>
+            <p>Television</p>
+        </div>
+        <div className="centreTypeCard">
+            <div>icon</div>
+            <p>Television</p>
+        </div>
+        <div className="centreTypeCard">
+            <div>icon</div>
+            <p>Television</p>
+        </div>
+        <div className="centreTypeCard">
+            <div>icon</div>
+            <p>Television</p>
+        </div>
+        <div className="centreTypeCard">
+            <div>icon</div>
+            <p>Television</p>
+        </div>
+        <div className="centreTypeCard">
+            <div>icon</div>
+            <p>Television</p>
+        </div>
+        <div className="centreTypeCard">
+            <div>icon</div>
+            <p>Television</p>
+        </div>
+        <div className="centreTypeCard">
+            <div>icon</div>
+            <p>Television</p>
+        </div>
+         
+    </div>
   </div>
 }
 
