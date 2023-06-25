ce5b0ba6fb3402528a600624bec1cf3494d2804e
Olusanya <olusanyajolaoluwa@gmail.com>
2023-06-25 19:55:20 +0100

    pages done

--- reservnow/app/host-an-eventcentre/page.jsx
+++ reservnow/app/host-an-eventcentre/page.jsx
@@ -6,11 +6,19 @@ import React, { useEffect } from "react";
 import Link from "next/link";
 import Overview from "@/components/Overview/Overview";
 import AboutCentreOverview from "@/components/About Centre Overview/AboutCentreOverview";
-
+import UploadGallery from "@/components/Upload Gallery/UploadGallery";
+import StandOut from "@/components/Stand Out/StandOut";
+import FinishSetup from "@/components/Finish Setup/FinishSetup";
+import Capacity from "@/components/Capacity/Capacity";
+import Legal from "@/components/Legal/Legal";
 import AmenityPicker from "@/components/Amenities/AmenityPicker";
 
 const HostAnEventCentrePage = ({}) => {
-  const [active, setActive] = useState("Overview");
+  const [active, setActive] = useState("Legal");
+  const [selectedFiles, setSelectedFiles] = useState([]);
+  const isSubmitDisabled = selectedFiles.length < 5;
+  const coverPhoto = selectedFiles.length > 0 ? selectedFiles[0] : null;
+  const remainingPhotos = selectedFiles.slice(1);
 
   return (
     <div className={styles["host-root"]}>
@@ -29,54 +37,111 @@ const HostAnEventCentrePage = ({}) => {
           <div className={styles["host-content"]}>
             {active === "Overview" && <Overview />}
             {active === "AboutCentreOverview" && <AboutCentreOverview />}
-            {active === "AmenityPicker" && <AmenityPicker/>}
-          
-
-
+            {active === "AmenityPicker" && <AmenityPicker />}
+            {active === "UploadGallery" && (
+              <UploadGallery
+                selectedFiles={selectedFiles}
+                setSelectedFiles={setSelectedFiles}
+                isSubmitDisabled={isSubmitDisabled}
+                coverPhoto={coverPhoto}
+                remainingPhotos={remainingPhotos}
+              />
+            )}
+            {active === "StandOut" && <StandOut />}
+            {active === "FinishSetup" && <FinishSetup />}
+            {active === "Capacity" && <Capacity />}
+            {active === "Legal" && <Legal />}
           </div>
         </div>
         <div className={styles["pagefooter-section"]}>
-        <div className={styles["page-loader"]}></div>
-        <div className={styles["host-footer"]}>
-          {active === "Overview" && <div></div>}
-          {active === "Overview" && (
-            <button onClick={() => setActive("AboutCentreOverview")}>
-              Get started
-            </button>
-          )}
-          {active === "AboutCentreOverview" && (
-            <div
-              className={styles["back-btn"]}
-              onClick={() => setActive("Overview")}
-            >
-              Back
-            </div>
-          )}
-          {active === "AboutCentreOverview" && (
-            <button
-              onClick={() => setActive("AmenityPicker")}
-              style={{ backgroundColor: "black", color: "white" }}
-            >
-              Next
-            </button>
-          )}
-           {active === "AmenityPicker" && (
-            <div
-              className={styles["back-btn"]}
-              onClick={() => setActive("AboutCentreOverview")}
-            >
-              Back
-            </div>
-          )}
-          {active === "AmenityPicker" && (
-            <button
-              onClick={() => setActive("AmenityPicker")}
-              style={{ backgroundColor: "black", color: "white" }}
-            >
-              Next
-            </button>
-          )}
-        </div>
+          <div className={styles["page-loader"]}></div>
+          <div className={styles["host-footer"]}>
+            {active === "Overview" && <div></div>}
+            {active === "Overview" && (
+              <div
+                onClick={() => setActive("AboutCentreOverview")}
+                className={styles["get-started-btn"]}
+              >
+                Get started
+              </div>
+            )}
+            {active === "AboutCentreOverview" && (
+              <div
+                className={styles["back-btn"]}
+                onClick={() => setActive("Overview")}
+              >
+                Back
+              </div>
+            )}
+            {active === "AboutCentreOverview" && (
+              <div
+                onClick={() => setActive("AmenityPicker")}
+                className={styles["next-btn"]}
+              >
+                Next
+              </div>
+            )}
+            {active === "AmenityPicker" && (
+              <div
+                className={styles["back-btn"]}
+                onClick={() => setActive("AboutCentreOverview")}
+              >
+                Back
+              </div>
+            )}
+            {active === "AmenityPicker" && (
+              <div
+                onClick={() => setActive("UploadGallery")}
+                className={styles["next-btn"]}
+              >
+                Next
+              </div>
+            )}
+            {active === "UploadGallery" && (
+              <div
+                className={styles["back-btn"]}
+                onClick={() => setActive("AmenityPicker")}
+              >
+                Back
+              </div>
+            )}
+            {active === "UploadGallery" && (
+              <div
+                onClick={() => !isSubmitDisabled && setActive("StandOut")}
+                className={`${styles["next-btn"]} ${
+                  isSubmitDisabled ? styles["disabled"] : ""
+                }`}
+              >
+                Next
+              </div>
+            )}
+            {active === "StandOut" && (
+              <div
+                className={styles["back-btn"]}
+                onClick={() => setActive("UploadGallery")}
+              >
+                Back
+              </div>
+            )}
+            {active === "StandOut" && (
+              <div onClick={() => setActive("")} className={styles["next-btn"]}>
+                Next
+              </div>
+            )}
+            {active === "FinishSetup" && (
+              <div
+                className={styles["back-btn"]}
+                onClick={() => setActive("UploadGallery")}
+              >
+                Back
+              </div>
+            )}
+            {active === "FinishSetup" && (
+              <div onClick={() => setActive("")} className={styles["next-btn"]}>
+                Next
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
--- reservnow/components/Upload Gallery/UploadGallery.jsx
+++ reservnow/components/Upload Gallery/UploadGallery.jsx
@@ -1,7 +1,236 @@
-import "./uploadGallery.css"
+"use client";
 
-const UploadGallery = ({}) => {
-  return <div className="upload-gallery-root"></div>
-}
+import "./uploadGallery.css";
+import React, { useState, useRef } from "react";
+import { IoImagesOutline } from "react-icons/io5";
+import { BsTrash } from "react-icons/bs";
+import { IoAddSharp } from "react-icons/io5";
 
-export default UploadGallery
+const UploadGallery = ({
+  selectedFiles,
+  setSelectedFiles,
+  isSubmitDisabled,
+  coverPhoto,
+  remainingPhotos,
+}) => {
+  const [isDragging, setIsDragging] = useState(false);
+  const fileInputRef = useRef(null);
+
+  const handleFileChange = (event) => {
+    const files = event.target.files;
+    setSelectedFiles((prevFiles) => [...prevFiles, ...files]);
+  };
+
+  const handleClick = () => {
+    fileInputRef.current.click();
+  };
+
+  const handleDragOver = (event) => {
+    event.preventDefault();
+    setIsDragging(true);
+  };
+
+  const handleDragLeave = (event) => {
+    event.preventDefault();
+    setIsDragging(false);
+  };
+
+  const handleDrop = (event) => {
+    event.preventDefault();
+    const files = event.dataTransfer.files;
+    setSelectedFiles([...files]);
+    setIsDragging(false);
+  };
+
+  const handleRemove = (index) => {
+    const updatedFiles = [...selectedFiles];
+    updatedFiles.splice(index, 1);
+    setSelectedFiles(updatedFiles);
+  };
+
+  const handleRemoveFirstPhoto = () => {
+    const updatedFiles = [...selectedFiles];
+    updatedFiles.splice(0, 1);
+    setSelectedFiles(updatedFiles);
+  };
+
+  const handleAddMore = () => {
+    fileInputRef.current.click();
+  };
+
+  const handleDragStart = (event, index) => {
+    event.dataTransfer.setData("text/plain", index);
+  };
+
+  const handleDragOverPhoto = (event) => {
+    event.preventDefault();
+  };
+
+  const handleDropPhoto = (event, index) => {
+    event.preventDefault();
+    const draggedIndex = event.dataTransfer.getData("text/plain");
+    const updatedFiles = [...selectedFiles];
+    const draggedFile = updatedFiles[draggedIndex];
+    updatedFiles.splice(draggedIndex, 1);
+    updatedFiles.splice(index, 0, draggedFile);
+    setSelectedFiles(updatedFiles);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (selectedFiles.length < 5) {
+      alert("Please select at least 5 files.");
+      return;
+    }
+
+    if (selectedFiles.length > 20) {
+      alert("Please select a maximum of 20 files.");
+      return;
+    }
+
+    // Reset the selected files
+    setSelectedFiles([]);
+  };
+
+  return (
+    <div className="upload-gallery-root">
+      {selectedFiles.length === 0 && (
+        <div className="upload-gallery-header-container">
+          <div className="upload-gallery-header">
+            Add some photos of your event centre
+          </div>
+          <div className="upload-gallery-sub-header">
+            You'll need 5 photos to get started. You can add more or make
+            changes later.
+          </div>
+        </div>
+      )}
+      <input
+        type="file"
+        multiple
+        ref={fileInputRef}
+        style={{ display: "none" }}
+        onChange={handleFileChange}
+      />
+      {selectedFiles.length === 0 ? (
+        <div
+          style={{
+            background: isDragging ? "#f7f7f7" : "#fff",
+            color: isDragging ? "black" : "#000",
+          }}
+          onDragOver={handleDragOver}
+          onDragLeave={handleDragLeave}
+          onDrop={handleDrop}
+          className="upload-photos"
+        >
+          <div className="img-icon">
+            <IoImagesOutline />
+          </div>
+          <div style={{ fontSize: "25px", fontWeight: "600" }}>
+            {isDragging ? "Drop the photo" : "Drag your photos here"}
+          </div>
+          <div style={{ marginTop: "2%", marginBottom: "5%" }}>
+            {isDragging ? "" : "Choose at least 5 photos"}
+          </div>
+          <div
+            style={{
+              fontWeight: "600",
+              textDecoration: "underline",
+              cursor: "pointer",
+            }}
+            onClick={handleClick}
+          >
+            Upload from your device
+          </div>
+        </div>
+      ) : (
+        <div className="preview-photo">
+          {coverPhoto && (
+            <div className="cover-photo">
+              <div className="header">
+                <div className="left">
+                  <h2>Choose at least 5 photos</h2>
+                  <p style={{ color: "grey", marginTop: "6px" }}>
+                    Drag to re-order
+                  </p>
+                </div>
+                <div className="right">
+                  <button onClick={handleAddMore}> + Add more</button>
+                </div>
+              </div>
+              <div
+                className="img-container"
+                onDragOver={handleDragOverPhoto}
+                onDrop={(event) => handleDropPhoto(event, 0)}
+              >
+                <div className="img-wrapper">
+                  <img
+                    src={URL.createObjectURL(coverPhoto)}
+                    alt="First Photo"
+                    style={{
+                      objectFit: "cover",
+                      cursor: "grab",
+                    }}
+                    draggable
+                    onDragStart={(event) => handleDragStart(event, 0)}
+                  />
+                  <div className="remove-btn">
+                    <div
+                      className="trash-icon"
+                      onClick={handleRemoveFirstPhoto}
+                    >
+                      <BsTrash />
+                    </div>
+                  </div>
+                  <div className="cover-photo-title">
+                    <div className="cover-photo-title-main">Cover Photo</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
+          {remainingPhotos.length > 0 && (
+            <div className="other-photos">
+              {remainingPhotos.map((file, index) => (
+                <div
+                  key={index}
+                  className="other-photos-container"
+                  onDragOver={handleDragOverPhoto}
+                  onDrop={(event) => handleDropPhoto(event, index + 1)}
+                >
+                  <img
+                    src={URL.createObjectURL(file)}
+                    alt={`Preview ${index + 1}`}
+                    style={{
+                      objectFit: "cover",
+                      cursor: "grab",
+                    }}
+                    draggable
+                    onDragStart={(event) => handleDragStart(event, index + 1)}
+                  />
+                  <div className="remove-btn1">
+                    <div
+                      className="trash-icon1"
+                      onClick={() => handleRemove(index + 1)}
+                    >
+                      <BsTrash />
+                    </div>
+                  </div>
+                </div>
+              ))}
+              <div className="add-more-box" onClick={handleAddMore}>
+                <div className="add-icon">
+                  <IoAddSharp />
+                </div>
+                Add more
+              </div>
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default UploadGallery;
