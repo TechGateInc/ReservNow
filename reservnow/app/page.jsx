cfedfe5e243d2e4aff2b27b085f84a1bea423a13
Emmanuel <alabiemmanuel177@gmail.com>
2023-08-08 11:01:29 +0100

    Updates to remove persist

--- reservnow/app/page.jsx
+++ reservnow/app/page.jsx
@@ -1,11 +1,7 @@
-import store, { persistor } from "@/store";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
+import { Providers } from "@/Provider";
 
-export default function Landing(){
-    return(
-        <Provider store={store}>
-                <PersistGate loading={null} persistor={persistor}></PersistGate>
-        </Provider>
+export default function Landing() {
+    return (
+        <Providers>        </Providers>
     )
 }
